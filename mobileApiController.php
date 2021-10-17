8881d39a83bb89cc8f9ac96f953cc92eadfe624c
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2021-10-17 17:16:40 +0100

    Debug labels

--- mobileApiController.php
+++ mobileApiController.php
@@ -68,8 +68,8 @@ class Mobile_Api_Controller
         $posts = get_posts($args);
 
         $data = [];
-        $data[] = $perPage;
-        $data[] = $page;
+        $data[] = "per".$perPage;
+        $data[] = "page".$page;
         if (empty($posts)) {
             return rest_ensure_response($data);
         }
